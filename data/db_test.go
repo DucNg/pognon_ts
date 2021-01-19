269f6e3efb305c9dfc397ed31e7992775c160d01
Duc Nguyen <nguyen@emersion.fr>
2021-01-19 15:50:01 +0100

    add update on database

--- data/db_test.go
+++ data/db_test.go
@@ -14,7 +14,8 @@ func TestDeletePerson(t *testing.T) {
 	t.Log("Creating a new pognon")
 
 	var pognon = Pognon{IDPognon: 1, PognonHash: "abcdefgh"}
-	var participants = []Person{{IDPerson: 1, Name: "toma", PognonHash: "abcdefgh"}}
+	var participants = []Person{{IDPerson: 1, Name: "toma", PognonHash: "abcdefgh"},
+		{IDPerson: 2, Name: "coucou", PognonHash: "abcdefgh"}}
 	var transaction = Transaction{
 		Buyers: []Purchase{{1, 10.25, false}},
 		For:    []Purchase{{1, 0, true}},
@@ -46,6 +47,19 @@ func TestDeletePerson(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log("Sucessfully added a transaction")
+
+	dbParticipants, err := GetParticipants(engine, "abcdefgh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("IDPerson 1 current name: ", (*dbParticipants)[0].Name)
+	t.Log("Changing IDPerson 1 name...")
+	PutPerson(engine, &Person{IDPerson: 1, Name: "jean"})
+	dbParticipants, err = GetParticipants(engine, "abcdefgh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("IDPerson 1 name is now: ", (*dbParticipants)[0].Name)
 
 	t.Log("Trying to delete IDPerson 1")
 	err = DeletePerson(engine, 1)
