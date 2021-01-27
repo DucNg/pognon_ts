394e58b74b81c6d5fabe206cb1240a383fb29c4b
Duc Nguyen <nguyen@emersion.fr>
2021-01-27 12:13:12 +0100

    display amounts on edit transaction

--- pognon-web-ui/src/AddTransactionDialog/SelectPersons.tsx
+++ pognon-web-ui/src/AddTransactionDialog/SelectPersons.tsx
@@ -20,8 +20,8 @@ interface item {
 
 function SelectPersons({type, participants, transaction, setTransaction, error, setError}: Props) {
     const [amounts, setAmounts] = useState({
-        Buyers: [""],
-        For: [""],
+        Buyers: transaction.Buyers.map(purchase => purchase.Amount.toFixed(2)),
+        For: transaction.For.map(purchase => purchase.Amount.toFixed(2)),
     })
 
     const handleChangeItems = (index: number, event: React.ChangeEvent<{name?: string; value: unknown;}>) => {
